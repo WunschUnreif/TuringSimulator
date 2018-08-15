b2f5be28b2839a6254903a0b74fa286f1eb8943b
WunschUnreif <30885930+WunschUnreif@users.noreply.github.com>
2018-08-15 22:14:01 +0800

    add code

---
+++ src/Machine.h
@@ -0,0 +1,52 @@
+#ifndef MACHINE_H
+#define MACHINE_H
+
+#include <iostream>
+#include <vector>
+#include <map>
+#include <tuple>
+#include <utility>
+#include <functional>
+#include <set>
+
+class Machine {
+public:
+    enum MachineFlag {
+        NORMAL,
+        ACCEPT,
+        REJECT,
+        UNDEF,
+        OVERFLOW
+    };
+
+private:
+    std::map<std::pair<int, char>, std::tuple<int, char, bool>> transFunc;
+    int maxLength;
+    int stateNum, acceptState, rejectState, startState;
+    std::function<void(const Machine&)> onAccept, onReject, onUndef, onOverflow;
+    std::function<void(const Machine&)> singleStepISR;
+
+    std::set<char> inputAlphabet, tapeAlphabet;
+
+public:
+    std::vector<char> tape;
+    std::vector<char>::iterator pos;
+    int currState;
+    MachineFlag mchFlage;
+
+public:
+    explicit Machine(const std::string& mchDscpFile);
+
+    void bindAccept(std::function<void(const Machine&)> onAcceptCb);
+    void bindReject(std::function<void(const Machine&)> onRejectCb);
+    void bindUndef(std::function<void(const Machine&)> onUndefCb);
+    void bindOverflow(std::function<void(const Machine&)> onOverflowCb);
+    void bindSigleStepISR(std::function<void(const Machine&)> ssISR);
+
+    void resetMachine();
+    void input(const std::string& inputTape);
+
+    void run();
+};
+
+#endif
